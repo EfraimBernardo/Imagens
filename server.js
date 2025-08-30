07fa1dab85799cf2af5104e1023e9064a6068738
TeuNome <teu@email.com>
2025-08-30 01:52:52 +0100

    salvando alterações locais

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const PORT = 3002;
+const PORT = 3003;
 
-// tua chave do Unsplash
-const UNSPLASH_ACCESS_KEY = 'LJ2bmJ3GbqClJiYXf8S9WFmnIbD4Nlt_3uwgV1Mn2Ck'; 
+// tua chave do Pixabay
+const PIXABAY_API_KEY = '52036501-4b182030a33836db1d49ce934';
 
 // servir arquivos da pasta public (HTML, CSS, JS)
 app.use(express.static('public'));
@@ -13,16 +13,18 @@ app.use(express.static('public'));
 app.get('/api/pesquisa', async (req, res) => {
   const { q } = req.query;
   try {
-    const response = await axios.get('https://api.unsplash.com/search/photos', {
+    const response = await axios.get('https://pixabay.com/api/', {
       params: {
-        query: q || 'nature', // se não passar nada, pesquisa "nature"
-        per_page: 30,
-      },
-      headers: { Authorization: `Client-ID ${LJ2bmJ3GbqClJiYXf8S9WFmnIbD4Nlt_3uwgV1Mn2Ck}` }
+        key: PIXABAY_API_KEY,
+        q: q || 'nature',   // se não passar nada, pesquisa "nature"
+        image_type: 'photo',
+        per_page: 30
+      }
     });
 
-    res.json(response.data.results);
+    res.json(response.data.hits); // no Pixabay é "hits", não "results"
   } catch (err) {
+    console.error(err);
     res.status(500).json({ error: 'Erro ao buscar imagens' });
   }
 });
